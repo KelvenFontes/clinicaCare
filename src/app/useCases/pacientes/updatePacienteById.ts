bb8e0504706b27a0d90299c52110fdea8a33a755
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-06-23 14:53:29 -0300

    feat: add function update user by cpf

--- src/app/useCases/pacientes/updatePacienteById.ts
+++ src/app/useCases/pacientes/updatePacienteById.ts
@@ -1,24 +1,46 @@
 import { Request, Response } from "express";
 import { Paciente } from "../../models/Paciente";
+import mongoose from "mongoose";
 
 export async function updatePaciente(req: Request, res: Response) {
   try {
     const { id } = req.params;
     const { nomeCompleto, dataNascimento, genero, endereco, contato, historicoMedico, idConsulta } = req.body;
 
-    const paciente = await Paciente.findByIdAndUpdate(
-      id,
-      {
-        nomeCompleto,
-        dataNascimento,
-        genero,
-        endereco,
-        contato,
-        historicoMedico,
-        idConsulta
-      },
-      { new: true, runValidators: true }
-    );
+    console.log('ID ou CPF recebido:', id); // Verifica o valor recebido para id ou cpf
+
+    let paciente;
+
+    // Verifica se o parâmetro é um ObjectId válido (caso seja id) e faz a atualização por id
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      paciente = await Paciente.findByIdAndUpdate(
+        id,
+        {
+          nomeCompleto,
+          dataNascimento,
+          genero,
+          endereco,
+          contato,
+          historicoMedico,
+          idConsulta
+        },
+        { new: true, runValidators: true }
+      );
+    } else { // Caso contrário, faz a atualização por cpf
+      paciente = await Paciente.findOneAndUpdate(
+        { cpf: id },
+        {
+          nomeCompleto,
+          dataNascimento,
+          genero,
+          endereco,
+          contato,
+          historicoMedico,
+          idConsulta
+        },
+        { new: true, runValidators: true }
+      );
+    }
 
     if (!paciente) {
       return res.status(404).json({ message: 'Paciente não encontrado' });
@@ -26,7 +48,7 @@ export async function updatePaciente(req: Request, res: Response) {
 
     res.status(200).json(paciente);
   } catch (error) {
-    console.error(error);
+    console.error('Erro ao atualizar paciente:', error);
     res.sendStatus(500);
   }
 }
